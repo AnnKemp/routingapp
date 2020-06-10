7eaac619c533829c9e2159f277c9dc4aa8adb64e
AnnKemp <ann.kemp@scarlet.be>
2020-06-10 12:05:13 +0200

    the menu works now showing the content of the pages . . .mmm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+//import { slideInAnimation } from './animations'; nog even uitzoeken hoe dat werkt
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //, animations: [ slideInAnimation ]
 })
 export class AppComponent {
   title = 'routingapp';
+
+  navbarOpen = false;
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+ // getAnimationData(outlet: RouterOutlet) {
+  //  return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation; // voor die animatie nog even uitzoeken
+  //}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap'; wat is dit even onderzoeken waar dit voor dient
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+//import { ProductsModule } from './products/products.module'; die moet ik nog in orde maken
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
+    // NgbModule, nog even uitzoeken waar dit voor dient
+    // ProductsModule,  // deze nog even in orde maken
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
+
