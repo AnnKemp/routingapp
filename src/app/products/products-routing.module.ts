1ce427ac4f3d2681f787cc915d843b42a5673450
AnnKemp <ann.kemp@scarlet.be>
2020-06-11 15:50:26 +0200

    added some comments

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -12,12 +12,12 @@ import { ProductListComponent } from './product-list/product-list.component';
 // ];
 const routes: Routes = [ // dus het 'submenu' wordt hier gebruikt om in layers dieper in de producten te duiken
   {
-    path: 'products',
+    path: 'products', // als je op products klikt krijg je de lijst met producten te zien
     component: ProductsComponent,
     children: [
       {
         path: '',
-        component: ProductListComponent,
+        component: ProductListComponent, // als je op een van de produktnamen klikt in deze lijst dan krijg de produkt-details te zien (maar weer terug op die lijst klikken om dan de gegevens van een ander product te zien te krijgen dat lukt dan weer niet, dus daar moet nog een andere oplossing voor worden gevonden)
         children: [
           {
             path: ':id',
